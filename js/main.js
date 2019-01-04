7b513cdf7714c9bd65bcc00591cb0141f4fe8386
Chesterrific <chesterguinto@gmail.com>
2019-01-03 16:23:04 -0800

    Changed projects page to sidebar. Added links to each project. Updated Resume.

--- js/main.js
+++ js/main.js
@@ -7,3 +7,13 @@ function closeNav() {
     var sidebar = document.getElementById("rightnav");
     sidebar.style.width = "0%";
 }
+
+function openProjects(){    
+    var sidebar = document.getElementById("projects");
+    sidebar.style.width = "25%";
+}
+
+function closeProjects(){
+    var sidebar = document.getElementById("projects");
+    sidebar.style.width = "0%";
+}

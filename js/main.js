9d5969c215d753b859f8dde225daefffe693fcd0
Chester Guinto <chesterguinto@gmail.com>
2020-08-25 16:29:22 -0700

    Combined js into one file. Added mouse cursor animations.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-
+  /* ------------------Quote JS------------------ */
   // Variables
   var timeBetweenQuotes = 4500;
 
@@ -34,5 +34,122 @@ $(document).ready(function () {
     $("#quoteBody").fadeIn("slow");
     $("#authorBody").fadeIn("slow");
     lastnum = quoteNum;
+  }
+
+  /* ------------------Nav JS------------------ */
+  // Variables
+  var scrollSpeed = 750,
+    topIsWhite = true,
+    botIsWhite = true,
+    topNav = document.getElementById('top'),
+    botNav = document.getElementById('bot'),
+    quoteEl = document.getElementById('transition');
+  aboutEl = document.getElementById('about');
+
+  $(window).bind('scroll', function () {
+    //Transition section's distance from top of window starting from its top.
+    var topOfQuote = quoteEl.offsetTop;
+
+    //Transition section's distance from top of window starting from its bottom.
+    var botOfQuote = quoteEl.offsetTop + quoteEl.offsetHeight;
+
+    //About section's distance from top of window starting from its top.
+    var topOfAbout = aboutEl.offsetTop;
+
+    //About section's distance from top of window starting from its bottom.
+    var botOfAbout = aboutEl.offsetTop + aboutEl.offsetHeight;
+
+    //TopNav's distance from top of window + half its height.
+    var topOfTNav = window.scrollY + (topNav.offsetTop + topNav.offsetHeight / 2);
+
+    //BotNav's distance from top of window + half its heigt.
+    var topOfBNav = window.scrollY + (botNav.offsetTop + botNav.offsetHeight / 2);
+
+    //Top nav color controls.
+    if (((botOfQuote >= topOfTNav && topOfTNav >= topOfQuote) || (botOfAbout >= topOfTNav && topOfTNav >= topOfAbout)) && topIsWhite === true) {
+      $('#top').css('color', 'black');
+      topIsWhite = false;
+    }
+    else if (!((botOfQuote >= topOfTNav && topOfTNav >= topOfQuote) || (botOfAbout >= topOfTNav && topOfTNav >= topOfAbout)) && topIsWhite === false) {
+      $('#top').css('color', 'white');
+      topIsWhite = true;
+    }
+
+    //Bot nav color controls.
+    if (((botOfQuote >= topOfBNav && topOfBNav >= topOfQuote) || (botOfAbout >= topOfBNav && topOfBNav >= topOfAbout)) && botIsWhite === true) {
+      $('#bot').css('color', 'black');
+      botIsWhite = false;
+    }
+    else if (!((botOfQuote >= topOfBNav && topOfBNav >= topOfQuote) || (topOfBNav >= topOfAbout)) && botIsWhite === false) {
+      $('#bot').css('color', 'white');
+      botIsWhite = true;
+    }
+  });
+
+  // Function Bindings
+  $("#projBtn").click(function () {
+    $('html, body').animate({
+      scrollTop: $('#projects').offset().top
+    }, scrollSpeed);
+  });
+
+  $("#aboutBtn").click(function () {
+    $('html, body').animate({
+      scrollTop: $('#about').offset().top
+    }, scrollSpeed);
+  });
+
+  $("#backBtn").click(function () {
+    $('html, body').animate({
+      scrollTop: 0
+    }, scrollSpeed);
+  });
+
+  /* ------------------Cursor Follow JS------------------ */
+  let cursor = document.getElementById('cursor');
+  let x = 0;
+  let y = 0;
+  let yscroll = 0;
+
+  let radius = 30;
+
+  // Cursor follow
+  document.addEventListener('mousemove', function (mousePos) {
+    x = mousePos.clientX;
+    y = mousePos.clientY;
+    yscroll = $(document).scrollTop();
+    cursor.style.left = x + 'px';
+    cursor.style.top = (y + yscroll) + 'px';
+  });
+
+  document.addEventListener('scroll', function () {
+    yscroll = $(document).scrollTop();
+    cursor.style.top = (y + yscroll) + 'px';
+  });
+
+  // Cursor size changes
+  $('a').hover(function () {
+    expandMouse();
+
+  }, function () {
+    resetMouseSize();
+  });
+
+  function expandMouse() {
+    $('#cursor').css({
+      'height': '100px',
+      'width': '100px',
+      'border' : '2px dashed #bfbfbf',
+      'animation' : 'spin 5s linear infinite'
+    });
+  }
+
+  function resetMouseSize() {
+    $('#cursor').css({
+      'height': radius + 'px',
+      'width': radius + 'px',
+      'border' : '2px solid #bfbfbf',
+      'animation' : 'none'
+    });
   }
 });
